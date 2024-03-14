970be4a82072fe77ea7b5b2f3729d15efc7fa46c
eric2969 <eric2969@gmail.com>
2024-03-14 02:50:33 +0000

    20240314 02:50:33

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 dir=`pwd`
-dest=$HOME"mc_server"
+dest=$HOME"/mc_server"
 echo "Copying Files..."
 if [[ $dir != $dest ]];then
     cp -R $dir ~/mc_server
 fi
+echo "Cloning scripts..."
 cd ~/mc_server
 cp ./mc_start.sh ~/mc_start.sh
 cp ./mc_backup.sh ~/mc_backup.sh
+echo "Setting Up crontab..."
 crontab -l | { cat; cat cron; } | crontab -
+echo "Clearing temporary files..."
 if [[ $dir != $dest ]];then
     rm -rf $dir
 fi
-echo "Setup Complete!!!"
+echo "Setup Complete!!!
+Please restart to apply the server !!!"
