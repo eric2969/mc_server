b808744f82b9a91b22e50525f8796a4479701799
eric2969 <eric2969@gmail.com>
2024-04-18 05:25:06 +0000

    20240418 05:25

--- mc_backup.sh
+++ mc_backup.sh
@@ -2,6 +2,6 @@
 cd ~/mc_server
 wall "Minecraft Server Backing up.."
 git add .
-git commit -m "`date +"%Y%m%d %H:%M:%S"`"
+git commit -m "`date +"%Y%m%d %H:%M"`"
 git push
 wall "Minecraft Server Backup Complete!!!"
