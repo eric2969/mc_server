b55e14a0ca71cfd13a595b6433a0dff5b64a6888
eric2969 <eric2969@gmail.com>
2025-06-25 02:00:21 +0800

    20250625 02:00

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 wall "Starting Minecraft Server..."
-java -Xmx16G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar server.jar nogui
+java -Xms4G -Xmx12G -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+     -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
+     -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 \
+     -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M \
+     -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 \
+     -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
+     -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem \
+     -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=true -Dcom.mojang.eula.agree=true \
+     -jar server.jar nogui
 wall "Minecraft Server Closed!!!"
